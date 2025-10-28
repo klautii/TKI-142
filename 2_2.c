ef0f538f8c9ae9f29d901dcdf86ec466c648e506
Дудорова Ульяна ТКИ-142 <uliana.dudorova@gmail.com>
2025-10-28 16:44:03 +0300

    2_2
    Загрузила решение задания 2_2

---
+++ 2_2.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+* @brief функция для вычисления значения а
+* @param count значение переменной cnt
+* @param f значение переменной f
+* @return результат  вычисления а
+*/
+
+double Input(void);
+/**
+* @brief вычисляет функцию
+* @param a параметр, данный в таблице
+* @param x значение, введенное пользователем
+* @return значение функции
+*/
+
+double y(const double a, const double x);
+
+/**
+* @brief точка входа в программу
+* @return 0 в случае успеха
+*/
+
+int main(void)
+{
+    const double a = 0.3;
+    puts("Введите значение x");
+    const double x = Input();
+    printf("Значение функции:%lf при x равном %lf", y(a, x), x);
+    return 0;
+}
+double Input(void)
+{
+    double value = 0.0;
+    if (scanf_s("%lf", &value) != 1)
+    {
+        printf("Не является числом");
+        exit(EXIT_FAILURE);
+    }
+    return value;
+}
+double y(const double a, const double x)
+{
+    if (a * x < 1)
+    {
+        return a * sin((pow(x, 2) + 1) / 10);
+    }
+    return a * cos((x - 1) / 10);
+}
