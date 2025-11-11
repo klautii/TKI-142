bc2798d1a5979024dd92e190a6d728f421714304
Дудорова Ульяна ТКИ-142 <uliana.dudorova@gmail.com>
2025-11-11 17:30:45 +0300

    Update 2_2.c
    исправила вывод ошибки и сравнении с числом

--- 2_2.c
+++ 2_2.c
@@ -1,52 +1,60 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 
 /**
-* @brief функция для вычисления значения а
-* @param count значение переменной cnt
-* @param f значение переменной f
-* @return результат  вычисления а
+* @brief Безопасный ввод числа
+* @return введенное число
 */
-
 double Input(void);
-/**
-* @brief вычисляет функцию
-* @param a параметр, данный в таблице
-* @param x значение, введенное пользователем
-* @return значение функции
-*/
 
+/**
+* @brief вычисляет функцию по заданной формуле
+* @param a параметр a = 0.3
+* @param x значение, введенное пользователем
+* @return значение функции y
+*/
 double y(const double a, const double x);
 
 /**
 * @brief точка входа в программу
 * @return 0 в случае успеха
 */
-
 int main(void)
 {
     const double a = 0.3;
-    puts("Введите значение x");
+
+    puts("Введите значение x:");
     const double x = Input();
-    printf("Значение функции:%lf при x равном %lf", y(a, x), x);
+    printf("Значение функции: %lf при x равном %lf\n", y(a, x), x);
+
     return 0;
 }
+
 double Input(void)
 {
     double value = 0.0;
     if (scanf_s("%lf", &value) != 1)
     {
-        printf("Не является числом");
+        fprintf(stderr, "Ошибка: введенное значение не является числом\n");
         exit(EXIT_FAILURE);
     }
     return value;
 }
+
 double y(const double a, const double x)
 {
-    if (a * x < 1)
+    double product = a * x;
+
+    if (product < 1.0 + DBL_EPSILON)
     {
         return a * sin((pow(x, 2) + 1) / 10);
     }
-    return a * cos((x - 1) / 10);
+    else
+    {
+
+        return a * cos((x - 1) / 10);
+    }
 }
+
