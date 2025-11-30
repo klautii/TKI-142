a2d2b9b09a5b562fc43acdcd83eafdfe8c7dc614
Дудорова Ульяна ТКИ-142 <uliana.dudorova@gmail.com>
2025-11-30 18:52:22 +0300

    Update 4_2.c
    исправила

--- 4_2.c
+++ 4_2.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
-#include <stdbool.h>
 
 /**
 * @brief Выделяет память под массив целых чисел.
@@ -19,12 +18,6 @@ int* IntArray(const size_t size);
 int InputArraySize(void);
 
 /**
- * @brief Получает выбор пользователя для способа заполнения массива.
- * @return Выбор пользователя.
- */
-int getFillChoice();
-
-/**
 * @brief Заполняет массив, считывая значения с клавиатуры.
 * @param arr Указатель на массив.
 * @param size Размер массива.
@@ -35,8 +28,6 @@ void FillingArrayKeyboard(int* arr, const size_t size);
 * @brief Заполняет массив случайными числами в заданном диапазоне.
 * @param arr Указатель на массив.
 * @param size Размер массива.
-* @param DownBound Нижняя граница случайных чисел.
-* @param UpBound Верхняя граница случайных чисел.
 */
 void FullRandomArray(int* arr, const size_t size, const int DownBound, const int UpBound);
 
@@ -60,16 +51,15 @@ int MinAbsNegativeElement(const int* arr, const size_t size);
  * @param arr Указатель на исходный массив.
  * @param arr2 Указатель на массив, в который будет скопирован результат замены.
  * @param size Размер исходного массива.
- * @return Новый массив после замены минимального по модулю отрицательного элемента.
  */
 void ReplaceTask1(const int* arr, int* arr2, const size_t size);
 
 /**
  * @brief Проверяет, оканчивается ли число на 0.
  * @param number Целое число, которое необходимо проверить.
- * @return true Если число оканчивается на 0, иначе false.
+ * @return 1 Если число оканчивается на 0, иначе 0.
  */
-bool EndsWithZero(int number);
+int EndsWithZero(int number);
 
 /**
  * @brief Подсчитывает количество элементов, которые не оканчиваются на 0.
@@ -84,7 +74,6 @@ size_t CountElementsNotEndWithZero(const int* arr, const size_t size);
  * @param arr Указатель на исходный массив.
  * @param arr2 Указатель на массив, в который будут скопированы элементы после удаления.
  * @param size Размер исходного массива.
- * @return Новый массив после удаления элементов, оканчивающихся на 0.
  */
 void RemoveElementsEndWithZero(const int* arr, int* arr2, const size_t size);
 
@@ -93,33 +82,14 @@ void RemoveElementsEndWithZero(const int* arr, int* arr2, const size_t size);
  * @param arr Указатель на исходный массив.
  * @param arr2 Указатель на массив, в который будет скопирован результат преобразования.
  * @param size Размер исходного массива.
- * @return Новый массив после преобразования.
  */
 void ChangeArray(const int* arr, int* arr2, const size_t size);
 
 /**
  * @brief Функция для безопасного ввода целого числа.
- * @param value Указатель на переменную, в которую будет записано введенное значение.
- * @param message Сообщение для пользователя.
- * @return true если ввод успешен, false в случае ошибки.
+ * @return Введенное целое число.
  */
-bool InputInt(int* value, const char* message);
-
-/**
- * @brief Функция для ввода границ диапазона случайных чисел с проверкой.
- * @param DownBound Указатель на нижнюю границу.
- * @param UpBound Указатель на верхнюю границу.
- * @return true если ввод успешен, false в случае ошибки.
- */
-bool InputBounds(int* DownBound, int* UpBound);
-
-/**
-* @brief Перечисление для выбора способа заполнения массива.
-*/
-enum TASK {
-    RANDOM,
-    KEYBOARD
-};
+int InputInt(void);
 
 /*
 * @brief Точка входа в программу.
@@ -129,39 +99,40 @@ int main(void)
 {
     srand(time(NULL));
     setlocale(LC_ALL, "Russian");
-    puts("Введите количество элементов в массиве: ");
-    size_t size = (size_t)InputArraySize();
 
+    size_t size = (size_t)InputArraySize();
     int* arr = IntArray(size);
 
-    // Выбор способа заполнения массива
     printf("\nКаким способом вы хотите заполнить массив?\n"
-        "Случайные числа - %d\n"
-        "Ввод с клавиатуры - %d\n", RANDOM, KEYBOARD);
+        "Случайные числа - 0\n"
+        "Ввод с клавиатуры - 1\n");
 
-    int choice = getFillChoice();
+    printf("Введите ваш выбор: ");
+    int choice = InputInt();
 
     switch (choice) {
-    case RANDOM: {
-        int DownBound = 0;
-        int UpBound = 0;
-        
-        if (!InputBounds(&DownBound, &UpBound)) {
-            fprintf(stderr, "Ошибка ввода границ!\n");
+    case 0: {
+        printf("Введите минимальную границу случайных чисел: ");
+        const int DownBound = InputInt();
+        printf("Введите максимальную границу случайных чисел: ");
+        const int UpBound = InputInt();
+
+        if (DownBound > UpBound) {
+            fprintf(stderr, "Ошибка: нижняя граница не может быть больше верхней!\n");
             free(arr);
-            exit(EXIT_FAILURE);
+            return 1;
         }
-        
+
         FullRandomArray(arr, size, DownBound, UpBound);
         break;
     }
-    case KEYBOARD:
+    case 1:
         FillingArrayKeyboard(arr, size);
         break;
     default:
-        fprintf(stderr, "Неправильный номер задания!\n");
+        fprintf(stderr, "Неправильный выбор!\n");
         free(arr);
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
     printf("\nИсходный массив: ");
@@ -171,7 +142,6 @@ int main(void)
     int* arr2 = IntArray(size);
     ReplaceTask1(arr, arr2, size);
     PrintArray(arr2, size);
-
     free(arr2);
 
     size_t newsize = CountElementsNotEndWithZero(arr, size);
@@ -179,48 +149,37 @@ int main(void)
     int* arr3 = IntArray(newsize);
     RemoveElementsEndWithZero(arr, arr3, size);
     PrintArray(arr3, newsize);
-
     free(arr3);
 
     printf("Массив M после преобразований: ");
     int* arr4 = IntArray(size);
     ChangeArray(arr, arr4, size);
     PrintArray(arr4, size);
-
     free(arr4);
-    free(arr);
 
+    free(arr);
     return 0;
 }
 
 int InputArraySize(void)
 {
-    int value;
+    int value = 0;
     printf("Введите размер массива: ");
     if (scanf("%d", &value) != 1 || value <= 0)
     {
-        fprintf(stderr, "Размер массива должен быть больше нуля.\n");
+        fprintf(stderr, "Ошибка: размер массива должен быть целым числом больше нуля.\n");
         exit(EXIT_FAILURE);
     }
-
     return value;
-}
-
-int getFillChoice()
-{
-    int input;
-    printf("Введите ваш выбор: ");
-    if (scanf("%d", &input) != 1)
-    {
-        fprintf(stderr, "Ошибка ввода выбора способа заполнения массива!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    return input;
 }
 
 int* IntArray(const size_t size)
 {
+    if (size == 0) {
+        fprintf(stderr, "Ошибка: размер массива не может быть нулевым\n");
+        exit(EXIT_FAILURE);
+    }
+
     int* arr = (int*)malloc(size * sizeof(int));
     if (arr == NULL)
     {
@@ -232,6 +191,11 @@ int* IntArray(const size_t size)
 
 void FullRandomArray(int* arr, const size_t size, const int DownBound, const int UpBound)
 {
+    if (arr == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в FullRandomArray\n");
+        return;
+    }
+
     for (size_t i = 0; i < size; i++)
     {
         arr[i] = rand() % (UpBound - DownBound + 1) + DownBound;
@@ -240,23 +204,26 @@ void FullRandomArray(int* arr, const size_t size, const int DownBound, const int
 
 void FillingArrayKeyboard(int* arr, const size_t size)
 {
+    if (arr == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в FillingArrayKeyboard\n");
+        return;
+    }
+
     printf("\nВведите %zu целых чисел для заполнения массива:\n", size);
     for (size_t i = 0; i < size; i++)
     {
-        int value = 0;
-        char message[50];
-        snprintf(message, sizeof(message), "Элемент [%zu]: ", i);
-        
-        if (!InputInt(&value, message)) {
-            fprintf(stderr, "Ошибка ввода элемента массива!\n");
-            exit(EXIT_FAILURE);
-        }
-        arr[i] = value;
+        printf("Элемент [%zu]: ", i);
+        arr[i] = InputInt();
     }
 }
 
 void PrintArray(const int* arr, const size_t size)
 {
+    if (arr == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в PrintArray\n");
+        return;
+    }
+
     for (size_t i = 0; i < size; i++)
     {
         printf("%d ", arr[i]);
@@ -266,6 +233,11 @@ void PrintArray(const int* arr, const size_t size)
 
 int MinAbsNegativeElement(const int* arr, const size_t size)
 {
+    if (arr == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в MinAbsNegativeElement\n");
+        return -1;
+    }
+
     int minIndex = -1;
     int minAbsValue = 0;
 
@@ -286,6 +258,11 @@ int MinAbsNegativeElement(const int* arr, const size_t size)
 
 void ReplaceTask1(const int* arr, int* arr2, const size_t size)
 {
+    if (arr == NULL || arr2 == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в ReplaceTask1\n");
+        return;
+    }
+
     int minIndex = MinAbsNegativeElement(arr, size);
 
     for (size_t i = 0; i < size; i++)
@@ -299,13 +276,18 @@ void ReplaceTask1(const int* arr, int* arr2, const size_t size)
     }
 }
 
-bool EndsWithZero(int number)
+int EndsWithZero(int number)
 {
     return (abs(number) % 10 == 0);
 }
 
 size_t CountElementsNotEndWithZero(const int* arr, const size_t size)
 {
+    if (arr == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в CountElementsNotEndWithZero\n");
+        return 0;
+    }
+
     size_t newSize = 0;
     for (size_t i = 0; i < size; i++) {
         if (!EndsWithZero(arr[i]))
@@ -313,12 +295,16 @@ size_t CountElementsNotEndWithZero(const int* arr, const size_t size)
             newSize++;
         }
     }
-
     return newSize;
 }
 
 void RemoveElementsEndWithZero(const int* arr, int* arr2, const size_t size)
 {
+    if (arr == NULL || arr2 == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в RemoveElementsEndWithZero\n");
+        return;
+    }
+
     size_t index = 0;
     for (size_t i = 0; i < size; i++)
     {
@@ -332,11 +318,16 @@ void RemoveElementsEndWithZero(const int* arr, int* arr2, const size_t size)
 
 void ChangeArray(const int* arr, int* arr2, const size_t size)
 {
+    if (arr == NULL || arr2 == NULL) {
+        fprintf(stderr, "Ошибка: передан NULL указатель в ChangeArray\n");
+        return;
+    }
+
     for (size_t i = 0; i < size; i++)
     {
         if (i % 2 == 0)
         {
-            arr2[i] = i * arr[i];
+            arr2[i] = (int)i * arr[i];
         }
         else
         {
@@ -345,35 +336,12 @@ void ChangeArray(const int* arr, int* arr2, const size_t size)
     }
 }
 
-bool InputInt(int* value, const char* message)
+int InputInt(void)
 {
-    if (message != NULL) {
-        printf("%s", message);
+    int value = 0;
+    if (scanf("%d", &value) != 1) {
+        fprintf(stderr, "Ошибка ввода числа!\n");
+        exit(EXIT_FAILURE);
     }
-    
-    if (scanf("%d", value) != 1) {
-        // Очистка буфера ввода в случае ошибки
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF);
-        return false;
-    }
-    return true;
-}
-
-bool InputBounds(int* DownBound, int* UpBound)
-{
-    if (!InputInt(DownBound, "Введите минимальную границу случайных чисел: ")) {
-        return false;
-    }
-    
-    if (!InputInt(UpBound, "Введите максимальную границу случайных чисел: ")) {
-        return false;
-    }
-    
-    if (*DownBound > *UpBound) {
-        fprintf(stderr, "Неправильно введена граница чисел!\n");
-        return false;
-    }
-    
-    return true;
+    return value;
 }
